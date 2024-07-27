760f9b88e8017f16fb2e80af5636f730d640863c
SanatielBorges <sanatielborges21@gmail.com>
2024-07-27 19:32:57 -0300

    compilando diversas mudanças no projeto

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,37 @@
 $(document).ready(function () {
   $(".multiple-items").slick({
-    infinite: true,
-    slidesToShow: 3,
+    slidesToShow: 3, // Exibe 3 slides por vez
     slidesToScroll: 1,
-    prevArrow: '<button class="slick-prev">←</button>',
-    nextArrow: '<button class="slick-next">→</button>',
+    prevArrow: '<div class="custom-prev"></div>',
+    nextArrow: '<div class="custom-next"></div>',
+    autoplaySpeed: 3000,
+    infinite: true,
+    responsive: [
+      {
+        breakpoint: 768, // Para tablets e telas menores
+        settings: {
+          slidesToShow: 2, // Exibe 2 slides
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480, // Para smartphones
+        settings: {
+          slidesToShow: 1, // Exibe 1 slide
+          slidesToScroll: 1,
+        },
+      },
+    ],
   });
 });
 
 // ação do menu hamburger
-document.addEventListener("DOMContentLoaded", function () {
-  const toggleButton = document.querySelector(".header__toggle");
-  const navMenu = document.querySelector(".header__nav");
 
-  toggleButton.addEventListener("click", function () {
+document.addEventListener("DOMContentLoaded", function () {
+  const menuHamburguer = document.querySelector(".menu-hamburguer");
+  const navMenu = document.querySelector(".header__nav ul");
+
+  menuHamburguer.addEventListener("click", function () {
     navMenu.classList.toggle("active");
   });
 });
