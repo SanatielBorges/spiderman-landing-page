a05ea02373c0013e644600e1a435da839a203cea
SanatielBorges <sanatielborges21@gmail.com>
2024-07-27 21:02:28 -0300

    atualizando gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -6,14 +6,12 @@ const htmlmin = require("gulp-htmlmin");
 const terser = require("gulp-terser");
 const concat = require("gulp-concat");
 
-// Compilação do Sass
 function compileSass() {
   return src("src/styles/main.scss")
     .pipe(sass().on("error", sass.logError))
     .pipe(dest("dist/css"));
 }
 
-// Minificação e renomeação do CSS
 function minifyCSS() {
   return src("dist/css/main.css")
     .pipe(cleanCSS())
@@ -21,14 +19,12 @@ function minifyCSS() {
     .pipe(dest("dist/css"));
 }
 
-// Minificação do HTML
 function minifyHTML() {
   return src("src/*.html")
     .pipe(htmlmin({ collapseWhitespace: true }))
     .pipe(dest("dist"));
 }
 
-// Concatenação e minificação do JavaScript
 function minifyJS() {
   return src("src/js/*.js")
     .pipe(concat("main.js"))
@@ -37,18 +33,13 @@ function minifyJS() {
     .pipe(dest("dist/js"));
 }
 
-// Tarefa para observar mudanças nos arquivos
+const build = series(compileSass, minifyCSS, minifyHTML, minifyJS);
+
 function watchFiles() {
   watch("src/styles/**/*.scss", compileSass);
   watch("src/js/*.js", minifyJS);
   watch("src/*.html", minifyHTML);
 }
 
-// Tarefa padrão
-exports.default = series(
-  compileSass,
-  minifyCSS,
-  minifyHTML,
-  minifyJS,
-  watchFiles
-);
+exports.default = series(build, watchFiles);
+exports.build = build;
